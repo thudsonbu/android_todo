9dd4dab111b8cc6ac958be9e570fbc482e954152
hudson_thom@bentley.edu <hudson_thom@bentley.edu>
2021-03-28 17:44:49 -0600

    list save succesful

--- app/src/main/java/com/course/example/todo/TodoList.java
+++ app/src/main/java/com/course/example/todo/TodoList.java
@@ -3,6 +3,7 @@ package com.course.example.todo;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -11,6 +12,9 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 
 public class TodoList extends AppCompatActivity implements AdapterView.OnItemClickListener {
@@ -19,6 +23,9 @@ public class TodoList extends AppCompatActivity implements AdapterView.OnItemCli
     public CustomAdapter todoAdapter;
     public EditText todoInput;
     public ListView todoList;
+    public OutputStreamWriter out;
+    public String fileName = "list.txt";
+    public File saveFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +42,21 @@ public class TodoList extends AppCompatActivity implements AdapterView.OnItemCli
 
         todoAdapter = new CustomAdapter(this, todoArray);
         todoList.setAdapter(todoAdapter);
+
+//        try {
+//            File ref = new File(fileName);
+//            if ( ref.exists() ) {
+//                readTodos();
+//            }
+//        } catch (Exception e){
+//            Log.e( "TodoList", e.getMessage() );
+//        }
+
+        try {
+            out = new OutputStreamWriter(openFileOutput(fileName, MODE_PRIVATE));
+        } catch (IOException e) {
+            Log.e( "TodoList", e.getMessage() );
+        }
     }
 
     @Override
@@ -51,6 +73,38 @@ public class TodoList extends AppCompatActivity implements AdapterView.OnItemCli
         todoInput.setText(todo);
         todoArray.remove(position);
         todoAdapter.notifyDataSetChanged();
+    }
+
+    // save list to 'list.txt' file
+    public void saveTodos(){
+        String todos = "";
+
+        if ( todoArray.size() == 0 ) {
+            Toast.makeText(this, "No Todo's to Save",
+                    Toast.LENGTH_SHORT)
+                    .show();
+            return;
+        }
+
+        try {
+            // delete old todos file
+//            if ( saveFile.exists() ) {
+//                saveFile.delete();
+//            }
+            
+            // create todo string
+            for ( Todo todo : todoArray ) {
+                todos += todo.getText().toString() + " \n";
+            }
+
+            out.write(todos);
+
+            out.close();
+
+            Toast.makeText(this, "List Saved Successfully", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            Toast.makeText(this, "Save Failed", Toast.LENGTH_SHORT).show();
+        }
     }
 
 
@@ -85,7 +139,7 @@ public class TodoList extends AppCompatActivity implements AdapterView.OnItemCli
                 return true;
 
             case R.id.save:
-                Toast.makeText(this, "List Saved Successfully", Toast.LENGTH_SHORT).show();
+                saveTodos();
                 return true;
 
             case R.id.close:
